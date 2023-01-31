a79a0917921e8209c03b38f8c8b3092f87d8b3c5
m-vishnuraj <mvishnurajm@gmail.com>
2023-01-31 06:03:40 +0530

    completed ui

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Account = () => {
     return (
-        <div>Account</div>
+        <>
+            <p className='text-gray-700 text-3xl mb-16 font-bold'>Account</p>
+            <div className='grid lg:grid-cols-3 gap-5 mb-16'>
+                <div className='bg-white rounded-lg shadow-lg p-5'></div>
+                <div className='bg-white rounded-lg shadow-lg p-5'></div>
+                <div className='bg-white rounded-lg shadow-lg p-5'></div>
+            </div>
+        </>
     )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>Dashboard</div>
+        <>
+            <p className='text-gray-700 text-3xl mb-16 font-bold'>Dashboard</p>
+            <div className='grid lg:grid-cols-3 gap-5 mb-16'>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+                <div className='rounded bg-white h-40 shadow-sm'></div>
+            </div>
+            <div className='grid col-1 bg-white h-96 shadow-sm'></div>
+        </>
     );
 };
 
